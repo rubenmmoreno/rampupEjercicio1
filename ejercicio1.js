0fbb057da1d37f78e46af564aee1c0065611e577
rubenmmoreno <rubenmbilbao@gmail.com>
2023-06-01 11:08:26 +0200

    second commit

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,26 +1,29 @@
-//Pedir entrada apropiada
-//Convertir frase a Array
-//Identificar coincidencias en el array con la entrada letra
 
-
-
-
-
-
-
+//Pedir entrada apropiada: frase y letra
+//Convertir frase a elementos de un Array. Usar método split()???
+//Recorrer array e identificar coincidencias con entrada letra
+//Crear un contador que aunmente en 1 ++ cada vez que se produzca un conteo positivo
+//Devolver contador
+//Introducir datos e invocar función con console.log()
 
 function cuentaLetras(frase, letra) {
-let letrasFrase = frase.split('');
-let posicion = 0;
-for (let x=0; x < letrasFrase.length; x++) {
-  if (letrasFrase[x] === letra) {
-    posicion++;
+let arrayLetras = frase.split("");
+let letrasEncontradas = 0;
+for (let x=0; x < arrayLetras.length; x++) {
+  if (arrayLetras[x] === letra) {
+    letrasEncontradas++;
   }
 }
-return posicion;
+return letrasEncontradas;
 }
+let frase = "Mi carro me lo robaron estando de romeria, ¿donde estará mi carro";
+let letra = "r";
+console.log(cuentaLetras(frase, letra));
 
-let frase = "He pasado por tu casa veinte veces";
-let letraSolucion = "p";
-let numeroVeces = cuentaLetras(frase, letraSolucion);
-console.log(numeroVeces);
+
+
+
+
+
+
+
